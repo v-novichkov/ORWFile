c2e3345debb0e9da775191399316e6f088c94423
Vladislav Novichkov <novichkov.vr@gmail.com>
2021-10-11 15:07:22 +0700

    Task 1

---
+++ main.py
@@ -0,0 +1,22 @@
+Recipes = "recipes.txt"
+cook_book = dict()
+
+
+def get_data(file_name):
+
+    with open(file_name) as file:
+        for line in file:
+            cook_name = line.strip()
+            amound_cook = int(file.readline())
+
+            temp_list = []
+            for item in range(amound_cook):
+                ingredient, quantity, measure = file.readline().split('|')
+                temp_list.append(
+                    {"ingredient_name": ingredient, "quantity": quantity, "measure": measure}
+                )
+            cook_book[cook_name] = temp_list
+
+            file.readline()
+    return data
+
