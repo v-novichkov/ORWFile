a0be6e77c548d4e81b984cbc6090fa478400cd19
Vladislav Novichkov <novichkov.vr@gmail.com>
2021-10-11 15:37:37 +0700

    Task 2

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ cook_book = dict()
 
 def get_data(file_name):
 
-    with open(file_name) as file:
+    with open(file_name, encoding="utf-8") as file:
         for line in file:
             cook_name = line.strip()
             amound_cook = int(file.readline())
@@ -21,4 +21,20 @@ def get_data(file_name):
             file.readline()
     return cook_book
 
+
 pprint(get_data(Recipes))
+
+
+def get_shop_list_by_dishes(dishes, person_count):
+    cooking_list = {}
+    for dish in dishes:
+        if dish in cook_book:
+            for ingr in cook_book[dish]:
+                if ingr['ingredient_name'] not in cooking_list:
+                    val = {'quantity': int(ingr['quantity']) * person_count, 'measure': ingr['measure']}
+                    cooking_list[ingr['ingredient_name']] = val
+                else:
+                    cooking_list[ingr['ingredient_name']]['quantity'] += int(ingr['quantity']) * person_count
+
+    return cooking_list
+
